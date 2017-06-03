36527648b2904949aeae7c2d8f422d5a6f173ddc
Tim Nguyen <tungnh91@gmail.com>
2017-06-03 15:21:25 -0700

    implemented a function to remove duplicates nodes

--- LinkedList/removeDups.js
+++ LinkedList/removeDups.js
@@ -5,5 +5,25 @@ function removeDups (list) => {
   if (!list) {
     return list;
   }
-  // 
+  // create a set for all the seen nodes
+  let seen = new Set();
+  // initiate the starting point
+  let node = list;
+  // add head to the set 
+  seen.add(node.val);
+  //slide down the list
+  while(node.next) {
+    // if the next node exist in the set
+    if (seen.has(node.next.val)) {
+      //skip it 
+      node.next = node.next.next;
+    } else {
+    // otherwise add it to the set
+      seen.add(node.next.val);
+      // then move on to the next one
+      node = node.next;
+    }
+  }
+  // return list, head will never change
+  return list;
 }
