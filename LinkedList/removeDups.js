209056f99e4a7cca881acc1c739f73ea9e283934
Tim Nguyen <tungnh91@gmail.com>
2017-06-05 15:32:21 -0700

    syntactic sugar

--- LinkedList/removeDups.js
+++ LinkedList/removeDups.js
@@ -1,29 +1,21 @@
 // Write a function to remove duplicates from an unsorted linked list.
-
-function removeDups (list) => {
-  // return input if falsy
+const removeDups = (list) => {
   if (!list) {
     return list;
-  }
-  // create a set for all the seen nodes
+  };
   let seen = new Set();
-  // initiate the starting point
-  let node = list;
-  // add head to the set 
-  seen.add(node.val);
-  //slide down the list
-  while(node.next) {
-    // if the next node exist in the set
-    if (seen.has(node.next.val)) {
-      //skip it 
-      node.next = node.next.next;
+  // add head to the current set
+  let currentNode = list;
+  seen.add(currentNode.value);
+  while (currentNode.next) {
+    if (seen.has(currentNode.next.value)) {
+      //skip
+      currentNode.next = currentNode.next.next;
     } else {
-    // otherwise add it to the set
-      seen.add(node.next.val);
-      // then move on to the next one
-      node = node.next;
+      // track & don't skip
+      seen.add(currentNode.next.value);
+      currentNode = currentNode.next;
     }
-  }
-  // return list, head will never change
+  };
   return list;
-}
+};
